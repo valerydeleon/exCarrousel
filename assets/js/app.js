8e5855754522683dd9cedc33394a46415c335e2b
Valeria de Leon <valery.deleon@gmail.com>
2017-05-29 16:57:33 -0500

    carrousel sin tiempo auto

---
+++ assets/js/app.js
@@ -0,0 +1,38 @@
+//almacenar carrousel en variable
+var carrousel = $("#carrousel");
+//almacenar botones de siguente y anterior
+var next = $("#btn-next");
+var previo = $("#btn-prev");
+
+//metodo insert affter - inserta un elemento despues del elemento indicado
+//mueve la ultima imagen al principio
+$("#carrousel div:last").insertBefore("#carrousel div:first");
+
+// se muestra la primera imagen con un margen de -100%
+carrousel.css("margin-left", '-'+100+'%');
+
+function moverDerecha(){
+    carrousel.animate({
+        marginLeft:"-"+200+"%"
+    } ,500, function(){
+        $("#carrousel div:first").insertAfter("#carrousel div:last");
+        carrousel.css("margin-left", '-'+100+'%'); //para que siempre muestre la siguiente
+    });  //animate-metodo de animacion 
+}
+
+function moverIzquierda(){
+    carrousel.animate({
+        marginLeft: 0
+    } ,500, function(){
+        $("#carrousel div:last").insertBefore("#carrousel div:first");
+        carrousel.css("margin-left", '-'+100+'%'); //para que siempre muestre la siguiente
+    });  //animate-metodo de animacion 
+}
+
+next.on("click", function() {
+    moverDerecha();
+});
+
+previo.on("click", function() {
+    moverIzquierda();
+});
